1afc322988fa08ab70d55013bdc402c5dff74162
Roman <romaniv788@gmail.com>
2020-03-21 16:07:01 +0200

    dsa

--- js/sort.js
+++
@@ -1,9 +0,0 @@
-class sort {
-    constructor() {
-    this.cartContainer = document.querySelector('#modal-cart');
-    this.updateBadge();
-    this.productService = new ProductsService();
-  ``}
-
-
-}
